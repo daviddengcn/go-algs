e97216990c1ed1f706606330589c97b01cea44fa
David <david@rd.netease.com>
2013-01-04 16:25:11 +0800

    MOD move maxflow package to maxflow folder

---
+++ maxflow/maxflow_test.go
@@ -0,0 +1,76 @@
+package maxflow
+
+import (
+    "testing"
+//    "fmt"
+)
+
+func o_() string {
+    pc, _, _, _ := runtime.Caller(1)
+    name := runtime.FuncForPC(pc).Name()
+    if p := strings.LastIndexAny(name, `./\`); p >= 0 {
+        name = name[p+1:]
+    } // if
+    fmt.Println("== BEGIN", name, "===")
+    return name
+}
+
+func __(name string) {
+    fmt.Println("== END", name, "===")
+}
+
+func assertResults(t *testing.T, expFlow CapType, g *Graph, nodes []*Node, isSources []bool) {
+    if g.Flow() != expFlow {
+        t.Errorf("Maxflow should be %d but get %d", expFlow, g.Flow())
+    } // if
+    
+    for i := range(nodes) {
+        if g.IsSource(nodes[i]) != isSources[i] {
+            if isSources[i] {
+                t.Errorf("node %d should be SOURCE!", i)
+            } else {
+                t.Errorf("node %d should be SINK!", i)
+            } // else
+        } // if
+    } // for i
+}
+
+func TestMaxflow1(t *testing.T) {
+    g := NewGraph()
+    
+    nodes := make([]*Node, 2)
+    
+    for i := range(nodes) {
+        nodes[i] = g.AddNode()
+    } // for i
+    
+    g.SetTweights(nodes[0], 1, 5)
+    g.SetTweights(nodes[1], 2, 6)
+    g.AddEdge(nodes[0], nodes[1], 3, 4)
+    
+    g.Run();
+    assertResults(t, 3, g, nodes, []bool{false, false})
+}
+
+func TestMaxflow2(t *testing.T) {
+    g := NewGraph()
+    
+    nodes := make([]*Node, 4)
+    
+    for i := range(nodes) {
+        nodes[i] = g.AddNode()
+    } // for i
+    
+    g.SetTweights(nodes[0], 3, 0)
+    g.SetTweights(nodes[1], 3, 0)
+    g.SetTweights(nodes[2], 0, 2)
+    g.SetTweights(nodes[3], 0, 3)
+    
+    g.AddEdge(nodes[0], nodes[1], 2, 0)
+    g.AddEdge(nodes[0], nodes[2], 3, 0)
+    g.AddEdge(nodes[1], nodes[3], 2, 0)
+    g.AddEdge(nodes[2], nodes[3], 4, 0)
+
+    g.Run();
+    assertResults(t, 5, g, nodes, []bool{true, true, false, false})
+}
