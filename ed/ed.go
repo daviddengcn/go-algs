f8dd9df8a9ae327e989ac4f721f9d4b3b606a970
David <david@rd.netease.com>
2013-01-05 15:55:20 +0800

    ADD edit-distance library.

--- ed/ed.go
+++ ed/ed.go
@@ -13,18 +13,21 @@ func min(a, b int) int {
     return b
 }
 
-func EditDistance(a, b string) int {
-    lb := utf8.RuneCountInString(b)
+/*
+String calculates the edit-distance between two strings. Input strings must be utf8 encoded.
 
-    f := make([]int, lb + 1)
+The time complexity is O(mn) where m and n are lengths of a and b, and space complexity is O(n).
+ */
+func String(a, b string) int {
+    f := make([]int, utf8.RuneCountInString(b) + 1)
     
-    for j := 0; j <= lb; j ++ {
+    for j := range(f) {
         f[j] = j
     } // for i
     
     for _, ca := range(a) {
         j := 1
-        fj1 := f[0] // the value of f[j - 1] in last iteration
+        fj1 := f[0] // fj1 is the value of f[j - 1] in last iteration
         f[0] ++
         for _, cb := range(b) {
             mn := min(f[j] + 1, f[j - 1] + 1) // delete & insert
@@ -36,6 +39,54 @@ func EditDistance(a, b string) int {
             
             fj1, f[j] = f[j], mn // save f[j] to fj1(j is about to increase), update f[j] to mn
             j ++
+        } // for cb
+    } // for ca
+    
+    return f[len(f) - 1]
+}
+
+/*
+Interface defines a pair of list and the cost for each operation.
+*/
+type Interface interface {
+    // LenA returns the lenght of the source list
+    LenA() int
+    
+    // LenB returns the lenght of the destination list
+    LenB() int
+    
+    // CostOfChange returns the change cost from an item in the source list at iA to an item in the destination list at iB
+    CostOfChange(iA, iB int) int
+    
+    // CostOfDel returns the cost of deleting an item in the source list at iA
+    CostOfDel(iA int) int
+    
+    // CostOfIns returns the cost of inserting an item in the destination list at iB
+    CostOfIns(iB int) int
+}
+
+/*
+EditDistance returns the edit-distance defined by Interface.
+
+The time complexity is O(mn) where m and n are lengths of a and b, and space complexity is O(n).
+*/
+func EditDistance(in Interface) int {
+    la, lb:= in.LenA(), in.LenB();
+    
+    f := make([]int, lb + 1)
+    
+    for j := 1; j <= lb; j ++ {
+        f[j] = f[j - 1] + in.CostOfIns(j - 1)
+    } // for i
+    
+    for i := 0; i < la; i ++ {
+        fj1 := f[0] // fj1 is the value of f[j - 1] in last iteration
+        f[0] += in.CostOfDel(i)
+        for j := 1; j <= lb; j ++ {
+            mn := min(f[j] + in.CostOfDel(i), f[j - 1] + in.CostOfIns(j - 1)) // delete & insert
+            mn = min(mn, fj1 + in.CostOfChange(i, j - 1)) // change/matched
+            
+            fj1, f[j] = f[j], mn // save f[j] to fj1(j is about to increase), update f[j] to mn
         } // for cb
     } // for ca
     
--- ed/ed_test.go
+++ ed/ed_test.go
@@ -21,18 +21,68 @@ func __(name string) {
     fmt.Println("== END", name, "===")
 }
 
-func TestEditDistance(t *testing.T) {
+func TestString(t *testing.T) {
     defer __(o_())
     
     cases := [][]interface{}{
-        {"abcd", "bcde", 2},
-        {"abcde", "", 5},
-        {"", "abcde", 5},
-        {"", "", 0},
+        { "abcd",  "bcde", 2},
+        {"abcde",      "", 5},
+        {     "", "abcde", 5},
+        {     "",      "", 0},
+        {"abcde", "abcde", 0},
         {"abcde", "dabce", 2}}
-    
+
     for _, cs := range(cases) {
-        d := EditDistance(cs[0].(string), cs[1].(string))
-        fmt.Println(d, cs[2].(int))
+        d := String(cs[0].(string), cs[1].(string))
+        if d != cs[2].(int) {
+            t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2], d)
+        } // if
+    } // for case
+}
+
+type stringInterface struct {
+    a, b []rune
+}
+
+func (in *stringInterface) LenA() int {
+    return len(in.a)
+}
+
+func (in *stringInterface) LenB() int {
+    return len(in.b)
+}
+
+func (in *stringInterface) CostOfChange(iA, iB int) int {
+    if in.a[iA] == in.b[iB] {
+        return 0
+    } // if
+    
+    return 100
+}
+
+func (in *stringInterface) CostOfDel(iA int) int {
+    return 100 + iA
+}
+
+func (in *stringInterface) CostOfIns(iB int) int {
+    return 110 + iB
+}
+
+func TestEditDistance(t *testing.T) {
+    defer __(o_())
+
+    cases := [][]interface{}{
+        { "abcd",  "bcde", 213},
+        {"abcde",      "", 510},
+        {     "", "abcde", 560},
+        {     "",      "", 0},
+        {"abcde", "abcde", 0},
+        {"abcde", "dabce", 213}}
+
+    for _, cs := range(cases) {
+        d := EditDistance(&stringInterface{[]rune(cs[0].(string)), []rune(cs[1].(string))})
+        if d != cs[2].(int) {
+            t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2].(int), d)
+        } // if
     } // for case
 }
