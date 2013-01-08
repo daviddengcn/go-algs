77af8a50115f4264221957ca13d8527cce06aec4
David <david@rd.netease.com>
2013-01-08 19:55:31 +0800

    ADD func based supporting and corresponding testcases ADD some helper funcs and types

--- ed/ed.go
+++ ed/ed.go
@@ -3,7 +3,9 @@ ed package provides some functions for generalized edit-distance calculation.
 
 String calculates the standard edit-distance, and StringFull returns an extra longest-common-string.
 
-EditDistance calculates the generalized edit-distance, and EditDistanceFull returns extra matching infomation.
+EditDistance calculates the generalized edit-distance, and EditDistanceFull returns extra matching infomation. Base is a helper type using as a base type for Interface implementations.
+
+EditDistanceF and EditDistanceFFull are similar to EditDistance and EditDistanceFull, but using parameter and functions instead of an interface. This is sometimes more easy to use. ConstCost is a helper function.
 */
 package ed
 
@@ -30,7 +32,7 @@ func String(a, b string) int {
     
     for j := range(f) {
         f[j] = j
-    } // for i
+    } // for j
     
     for _, ca := range(a) {
         j := 1
@@ -52,6 +54,14 @@ func String(a, b string) int {
     return f[len(f) - 1]
 }
 
+// Constants of operations
+const(
+    opDEL byte = iota
+    opINS
+    opCHANGE
+    opMATCH
+)
+    
 /*
 String calculates the edit-distance and longest-common-string between two strings. Input strings must be UTF-8 encoded.
 
@@ -64,16 +74,9 @@ func StringFull(a, b string) (dist int, lcs string) {
     f := make([]int, lb + 1)
     ops := make([]byte, la * lb)
     
-    const(
-        DEL byte = iota
-        INS
-        CHANGE
-        MATCH
-    )
-    
     for j := range(f) {
         f[j] = j
-    } // for i
+    } // for j
     
     p := 0; // the index to ops
     
@@ -82,22 +85,21 @@ func StringFull(a, b string) (dist int, lcs string) {
         fj1 := f[0] // fj1 is the value of f[j - 1] in last iteration
         f[0] ++
         for _, cb := range(b) {
-            mn, op := f[j] + 1, DEL
+            mn, op := f[j] + 1, opDEL
             if f[j - 1] + 1 < mn {
-                mn, op = f[j - 1] + 1, INS
+                mn, op = f[j - 1] + 1, opINS
             }// if
 
             if cb != ca {
                 if fj1 + 1 < mn {
-                    mn, op = fj1 + 1, CHANGE
+                    mn, op = fj1 + 1, opCHANGE
                 } // if
             } else {
-                    mn, op = fj1, MATCH
+                mn, op = fj1, opMATCH
             } // else
             
             fj1, f[j], ops[p] = f[j], mn, op // save f[j] to fj1(j is about to increase), update f[j] to mn
-            p ++
-            j ++
+            p ++;  j ++
         } // for cb
     } // for ca
     
@@ -105,23 +107,23 @@ func StringFull(a, b string) (dist int, lcs string) {
     lcsi := make([]int, 0, la)
     for i, j := la, lb; i > 0 || j > 0; {
         var op byte
-        if i == 0 {
-            op = INS
-        } else if j == 0 {
-            op = DEL
-        } else {
-            op = ops[(i - 1)*lb + j - 1]
-        } // else
+        switch {
+            case i == 0:
+                op = opINS
+            case j == 0:
+                op = opDEL
+            default:
+                op = ops[(i - 1)*lb + j - 1]
+        } // switch
         
         switch op {
-            case INS:
+            case opINS:
                 j --
-            case DEL:
+            case opDEL:
                 i --
-            case CHANGE, MATCH:
-                i --
-                j --
-                if op == MATCH {
+            case opCHANGE, opMATCH:
+                i --;  j --
+                if op == opMATCH {
                     lcsi = append(lcsi, i)
                 } // if
         } // switch
@@ -138,9 +140,52 @@ func StringFull(a, b string) (dist int, lcs string) {
             } // if
             i ++
         } // for ca
-    } // for  l
+    } // if
     
     return f[len(f) - 1], lcs
+}
+
+// Ternary returns vT if cond equals true, or vF otherwise.
+func Ternary(cond bool, vT, vF int) int {
+    if cond {
+        return vT
+    } // if
+    
+    return vF
+}
+
+// ConstCost returns a const-function whose return value is the specified cost. This is useful for EditDistanceF and EditDistanceFFull if the del/ins cost is constant to positions.
+func ConstCost(cost int) func(int) int {
+    return func(int) int {
+        return cost
+    }
+}
+
+/*
+Base is a helper type which defines LenA, LenB, CostOfDel and CostOfIns methods.
+*/
+type Base struct {
+    LA, LB, Cost int
+}
+
+// Interface.LenA
+func (b* Base) LenA() int {
+    return b.LA
+}
+
+// Interface.LenB
+func (b* Base) LenB() int {
+    return b.LB
+}
+
+// Interface.CostOfDel
+func (b* Base) CostOfDel(iA int) int {
+    return b.Cost
+}
+
+// Interface.CostOfIns
+func (b* Base) CostOfIns(iB int) int {
+    return b.Cost
 }
 
 /*
@@ -175,7 +220,7 @@ func EditDistance(in Interface) int {
     
     for j := 1; j <= lb; j ++ {
         f[j] = f[j - 1] + in.CostOfIns(j - 1)
-    } // for i
+    } // for j
 
     for i := 0; i < la; i ++ {
         fj1 := f[0] // fj1 is the value of f[j - 1] in last iteration
@@ -185,15 +230,44 @@ func EditDistance(in Interface) int {
             mn = min(mn, fj1 + in.CostOfChange(i, j - 1)) // change/matched
             
             fj1, f[j] = f[j], mn // save f[j] to fj1(j is about to increase), update f[j] to mn
-        } // for cb
-    } // for ca
+        } // for j
+    } // for i
     
     return f[lb]
 }
 
+func matchingFromOps(la, lb int, ops []byte) (matA, matB []int) {
+    matA, matB = make([]int, la), make([]int, lb)
+    for i, j := la, lb; i > 0 || j > 0; {
+        var op byte
+        switch {
+            case i == 0:
+                op = opINS
+            case j == 0:
+                op = opDEL
+            default:
+                op = ops[(i - 1)*lb + j - 1]
+        } // switch
+        
+        switch op {
+            case opINS:
+                j --
+                matB[j] = -1
+            case opDEL:
+                i --
+                matA[i] = -1
+            case opCHANGE:
+                i --;  j --
+                matA[i], matB[j] = j, i
+        } // switch
+    } // for i, j
+    
+    return matA, matB
+}
+
 /*
 EditDistanceFull returns the edit-distance and corresponding match indexes defined by Interface.
-Each element in matA and matB is the index in the other list, if it is equal to or greater than zero; or -1 meaning a deleting or inserting in matA or matB respectively.
+Each element in matA and matB is the index in the other list, if it is equal to or greater than zero; or -1 meaning a deleting or inserting in matA or matB, respectively.
 
 The time and space complexity are all O(mn) where m and n are lengths of a and b.
 
@@ -207,13 +281,7 @@ func EditDistanceFull(in Interface) (dist int, matA, matB []int) {
     
     for j := 1; j <= lb; j ++ {
         f[j] = f[j - 1] + in.CostOfIns(j - 1)
-    } // for i
-    
-    const(
-        DEL byte = iota
-        INS
-        CHANGE
-    )
+    } // for j
     
     // Matching with dynamic programming
     p := 0
@@ -221,48 +289,100 @@ func EditDistanceFull(in Interface) (dist int, matA, matB []int) {
         fj1 := f[0] // fj1 is the value of f[j - 1] in last iteration
         f[0] += in.CostOfDel(i)
         for j := 1; j <= lb; j ++ {
-            mn, op := f[j] + in.CostOfDel(i), DEL // delete
+            mn, op := f[j] + in.CostOfDel(i), opDEL // delete
             
             if v := f[j - 1] + in.CostOfIns(j - 1); v < mn {
                 // insert
-                mn, op = v, INS
+                mn, op = v, opINS
             } // if
             
              // change/matched
             if v := fj1 + in.CostOfChange(i, j - 1); v < mn {
                 // insert
-                mn, op = v, CHANGE
+                mn, op = v, opCHANGE
             } // if
             
             fj1, f[j], ops[p] = f[j], mn, op // save f[j] to fj1(j is about to increase), update f[j] to mn
             p ++
-        } // for cb
-    } // for ca
+        } // for j
+    } // for i
     // Reversely find the match info
-    matA, matB = make([]int, la), make([]int, lb)
-    for i, j := la, lb; i > 0 || j > 0; {
-        var op byte
-        if i == 0 {
-            op = INS
-        } else if j == 0 {
-            op = DEL
-        } else {
-            op = ops[(i - 1)*lb + j - 1]
-        } // else
-        
-        switch op {
-            case INS:
-                j --
-                matB[j] = -1
-            case DEL:
-                i --
-                matA[i] = -1
-            case CHANGE:
-                i --
-                j --
-                matA[i], matB[j] = j, i
-        } // switch
-    } // for i, j
+    matA, matB = matchingFromOps(la, lb, ops)
+    
+    return f[lb], matA, matB
+}
+
+/*
+EditDistanceFunc returns the edit-distance defined by parameters and functions.
+
+The time complexity is O(mn) where m and n are lengths of a and b, and space complexity is O(n).
+*/
+func EditDistanceF(lenA, lenB int, costOfChange func(iA, iB int) int, costOfDel func(iA int) int, costOfIns func(iB int) int)int {
+    la, lb:= lenA, lenB;
+    
+    f := make([]int, lb + 1)
+    
+    for j := 1; j <= lb; j ++ {
+        f[j] = f[j - 1] + costOfIns(j - 1)
+    } // for j
+
+    for i := 0; i < la; i ++ {
+        fj1 := f[0] // fj1 is the value of f[j - 1] in last iteration
+        f[0] += costOfDel(i)
+        for j := 1; j <= lb; j ++ {
+            mn := min(f[j] + costOfDel(i), f[j - 1] + costOfIns(j - 1)) // delete & insert
+            mn = min(mn, fj1 + costOfChange(i, j - 1)) // change/matched
+            
+            fj1, f[j] = f[j], mn // save f[j] to fj1(j is about to increase), update f[j] to mn
+        } // for j
+    } // for i
+    
+    return f[lb]
+}
+
+/*
+EditDistanceFFull returns the edit-distance and corresponding match indexes defined by parameters and functions.
+Each element in matA and matB is the index in the other list, if it is equal to or greater than zero; or -1 meaning a deleting or inserting in matA or matB, respectively.
+
+The time and space complexity are all O(mn) where m and n are lengths of a and b.
+
+NOTE if detailed matching information is not necessary, call EditDistance instead because it needs much less memories.
+*/
+func EditDistanceFFull(lenA, lenB int, costOfChange func(iA, iB int) int, costOfDel func(iA int) int, costOfIns func(iB int) int) (dist int, matA, matB []int) {
+    la, lb:= lenA, lenB;
+    
+    f := make([]int, lb + 1)
+    ops := make([]byte, la * lb)
+    
+    for j := 1; j <= lb; j ++ {
+        f[j] = f[j - 1] + costOfIns(j - 1)
+    } // for j
+    
+    // Matching with dynamic programming
+    p := 0
+    for i := 0; i < la; i ++ {
+        fj1 := f[0] // fj1 is the value of f[j - 1] in last iteration
+        f[0] += costOfDel(i)
+        for j := 1; j <= lb; j ++ {
+            mn, op := f[j] + costOfDel(i), opDEL // delete
+            
+            if v := f[j - 1] + costOfIns(j - 1); v < mn {
+                // insert
+                mn, op = v, opINS
+            } // if
+            
+             // change/matched
+            if v := fj1 + costOfChange(i, j - 1); v < mn {
+                // insert
+                mn, op = v, opCHANGE
+            } // if
+            
+            fj1, f[j], ops[p] = f[j], mn, op // save f[j] to fj1(j is about to increase), update f[j] to mn
+            p ++
+        } // for j
+    } // for i
+    // Reversely find the match info
+    matA, matB = matchingFromOps(la, lb, ops)
     
     return f[lb], matA, matB
 }
--- ed/ed_test.go
+++ ed/ed_test.go
@@ -21,6 +21,19 @@ func __(name string) {
     fmt.Println("== END", name, "===")
 }
 
+type unitStr struct {
+    Base
+    a, b []rune
+}
+
+func (us *unitStr) CostOfChange(iA, iB int) int {
+    if us.a[iA] == us.b[iB] {
+        return 0
+    } // if
+    
+    return 1
+}
+
 func TestString(t *testing.T) {
     defer __(o_())
     
@@ -35,6 +48,14 @@ func TestString(t *testing.T) {
 
     for _, cs := range(cases) {
         d := String(cs[0].(string), cs[1].(string))
+        if d != cs[2].(int) {
+            t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2], d)
+        } // if
+    } // for case
+    
+    for _, cs := range(cases) {
+        a, b := []rune(cs[0].(string)), []rune(cs[1].(string))
+        d := EditDistance(&unitStr{Base{len(a), len(b), 1}, a, b})
         if d != cs[2].(int) {
             t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2], d)
         } // if
@@ -147,3 +168,88 @@ func TestEditDistanceFull(t *testing.T) {
         } // if
     } // for case
 }
+
+func TestEditDistanceF(t *testing.T) {
+    defer __(o_())
+
+    cases := [][]interface{}{
+        { "abcd",  "bcde", 213},
+        {"abcde",      "", 510},
+        {     "", "abcde", 560},
+        {     "",      "", 0},
+        {"abcde", "abcde", 0},
+        {"abcde", "dabce", 213},
+        {"abcde", "abfde", 100}}
+
+    for _, cs := range(cases) {
+        a, b := []rune(cs[0].(string)), []rune(cs[1].(string))
+        d := EditDistanceF(len(a), len(b),
+            func(iA, iB int) int {
+                if a[iA] == b[iB] {
+                    return 0
+                } // if
+                
+                return 100
+            },
+            func(iA int) int {
+                return 100 + iA
+            },
+            func(iB int) int {
+                return 110 + iB
+            })
+        if d != cs[2].(int) {
+            t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2].(int), d)
+        } // if
+    } // for case
+}
+
+func TestEditDistanceFFull(t *testing.T) {
+    defer __(o_())
+
+    cases := [][]interface{}{
+        { "abcd",  "bcde", 213, []int{-1, 0, 1, 2}, []int{1, 2, 3, -1}},
+        {"abcde",      "", 510, []int{-1, -1, -1, -1, -1}, []int{}},
+        {     "", "abcde", 560, []int{}, []int{-1, -1, -1, -1, -1}},
+        {     "",      "", 0, []int{}, []int{}},
+        {"abcde", "abcde", 0, []int{0, 1, 2, 3, 4}, []int{0, 1, 2, 3, 4}},
+        {"abcde", "dabce", 213, []int{1, 2, 3, -1, 4}, []int{-1, 0, 1, 2, 4}},
+        {"abcde", "abfde", 100, []int{0, 1, 2, 3, 4}, []int{0, 1, 2, 3, 4}}}
+
+    for _, cs := range(cases) {
+        a, b := []rune(cs[0].(string)), []rune(cs[1].(string))
+        d, matA, matB := EditDistanceFFull(len(a), len(b),
+            func(iA, iB int) int {
+                if a[iA] == b[iB] {
+                    return 0
+                } // if
+                
+                return 100
+            },
+            func(iA int) int {
+                return 100 + iA
+            },
+            func(iB int) int {
+                return 110 + iB
+            })
+        if d != cs[2].(int) {
+            t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2].(int), d)
+        } // if
+        if fmt.Sprint(matA) != fmt.Sprint(cs[3]) {
+            t.Errorf("matA for matchting between %s and %s is expected to be %v, but %v got!", cs[0], cs[1], cs[3], matA)
+        } // if
+        if fmt.Sprint(matB) != fmt.Sprint(cs[4]) {
+            t.Errorf("matB for matchting between %s and %s is expected to be %v, but %v got!", cs[0], cs[1], cs[4], matB)
+        } // if
+    } // for case
+}
+
+func ExampleEditDistanceF() {
+    a, b := "abcd", "bcde"
+    d := EditDistanceF(len(a), len(b), func(iA, iB int) int {
+            return Ternary(a[iA] == b[iB], 0, 1)
+        }, ConstCost(1), ConstCost(1))
+    fmt.Println(a, b, d)
+    // Output:
+    // abcd bcde 2
+}
+
